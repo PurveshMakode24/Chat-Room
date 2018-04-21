61b285fc890e863f5ce2f0759404555164835a93
Purvesh Makode <makodepurvesh@gmail.com>
2018-04-21 23:26:56 +0530

    database backup added

---
+++ Chat_Room/database/messenger.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 21, 2018 at 07:49 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `messenger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shouts`
+--
+
+CREATE TABLE `shouts` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shouts`
+--
+
+INSERT INTO `shouts` (`id`, `user`, `message`, `time`) VALUES
+(1, 'Purvesh', 'Happy Birthday!', '10:05:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shouts`
+--
+ALTER TABLE `shouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shouts`
+--
+ALTER TABLE `shouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
